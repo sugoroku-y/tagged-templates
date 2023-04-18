a811f331f9e73bd6f9834fbf302d74157cf74763
YEBISUYA Sugoroku <yebisuya@gmail.com>
2023-04-19 07:43:15 +0900

    refactor: 関数名がつくように修正

--- src/error.ts
+++ src/error.ts
@@ -1,21 +1,16 @@
 import { indented } from './indented';
 
-/**
- * 引数に渡されたメッセージをもとに例外クラスERRORのインスタンスを作成してthrowする。
- * @template ERROR 例外クラス
- * @param clazz throwする例外クラスのコンストラクタ
- * @param args メッセージを生成するためのテンプレート引数
- */
-function error_implement<ERROR extends object>(
+function prepare<ERROR extends object>(
   clazz: new (message?: string) => ERROR,
-  ...args: [TemplateStringsArray, ...unknown[]]
-): never {
-  // エラーメッセージを含むERRORのインスタンスを作成する
-  const ex = new clazz(indented.safe(...args));
-  // スタックトレースからこの関数を除去する
-  Error.captureStackTrace(ex, error_implement);
-  // 作成したインスタンスをthrowする
-  throw ex;
+): (template: TemplateStringsArray, ...values: unknown[]) => never {
+  return function error(...args: [TemplateStringsArray, ...unknown[]]): never {
+    // エラーメッセージを含むERRORのインスタンスを作成する
+    const ex = new clazz(indented.safe(...args));
+    // スタックトレースからこの関数を除去する
+    Error.captureStackTrace(ex, error);
+    // 作成したインスタンスをthrowする
+    throw ex;
+  };
 }
 
 /**
@@ -49,10 +44,10 @@ export const error: {
   ): (...args: [TemplateStringsArray, ...unknown[]]) => never;
 } = Object.freeze(
   // error_implement関数をベースにerror関数を定義する
-  Object.assign(error_implement.bind(null, Error), {
+  Object.assign(prepare(Error), {
     // error_implement関数の第1引数にclazzを指定した関数を返す
     for<ERROR extends object>(clazz: new (message?: string) => ERROR) {
-      return error_implement.bind(null, clazz);
+      return prepare(clazz);
     },
   }),
 );
