c05d9d74831ec30def9fc17f411e435096c73351
YEBISUYA Sugoroku <yebisuya@gmail.com>
2023-05-04 19:06:26 +0900

    refactor: regexpをtaggedTemplateBaseを使った実装に書き換え

--- src/regexp.ts
+++ src/regexp.ts
@@ -1,3 +1,5 @@
+import { taggedTemplateBase } from './taggedTemplateBase';
+
 type AllCombinations<
   STR extends string,
   PRE extends string = '',
@@ -9,6 +11,25 @@ type AllCombinations<
 
 // 正規表現のフラグに指定できる文字列
 type RegExpFlags = AllCombinations<'dgimsuy'>;
+
+type RegExpLike = {
+  source: string;
+  flags: RegExpFlags;
+};
+
+type PickOnly<T, K extends keyof T> = Pick<T, K> &
+  Partial<Record<Exclude<keyof T, K>, never>>;
+
+type RegExpTemplateParam =
+  // 文字列値は正規表現の特殊文字をエスケープして挿入
+  | string
+  // `source`プロパティがあるオブジェクトはその`source`プロパティを`(?:～)`で囲って挿入
+  // `flags`プロパティがあるオブジェクトはその`flags`プロパティをマージしたものをフラグとして使用する。
+  | { [K in keyof RegExpLike]: PickOnly<RegExpLike, K> }[keyof RegExpLike]
+  // 両方あってもいい
+  | RegExpLike
+  // もちろん正規表現もOK
+  | RegExp;
 
 /**
  * 正規表現を生成するタグ付きテンプレート
@@ -23,25 +44,16 @@ type RegExpFlags = AllCombinations<'dgimsuy'>;
  * - 改行、空白、タブは除去する。
  *   - ただし、前後に英数字がある場合は`\s+`に置き換える。
  * - エスケープされている場合(`\/*`や`\ `など)は上記の対象外
- * @param {Array<string | object>} values テンプレートに挿入される値
+ * @param {RegExpTemplateParam} values テンプレートに挿入される値
  * - 文字列値が正規表現の特殊文字を含んでいる場合、エスケープしてから挿入。
- * - `source`プロパティを持つオブジェクトは、その`source`プロパティを`(?:～)`で囲って挿入。
- * - `flags`プロパティを持つオブジェクトは、そのすべての`flags`プロパティをマージしたものを生成するRegExpに指定。
+ * - `source`プロパティが文字列値であるオブジェクトは、その`source`プロパティを`(?:～)`で囲って挿入。
+ * - `flags`プロパティが文字列値であるオブジェクトは、そのすべての`flags`プロパティをマージしたものを生成するRegExpに指定。
  * - 上記以外は無視。
  * @returns {RegExp} テンプレート文字列と挿入される値から生成したパターンとフラグを指定して生成された正規表現を返す。
  */
 export function regexp(
   template: TemplateStringsArray,
-  ...values: Array<
-    // 文字列値は正規表現の特殊文字をエスケープして挿入
-    | string
-    // `source`プロパティがあるオブジェクトはその`source`プロパティを`(?:～)`で囲って挿入
-    | { source: string }
-    // `flags`プロパティがあるオブジェクトはその`flags`プロパティをマージしたものをフラグとして使用する。
-    | { flags: RegExpFlags }
-    // 両方あってもいい
-    | { source: string; flags?: RegExpFlags }
-  >
+  ...values: RegExpTemplateParam[]
 ): RegExp {
   const { source, flags } = regexp.sub(template, ...values);
   return new RegExp(source, flags);
@@ -52,12 +64,14 @@ export function regexp(
  *
  * `source`を正規表現として解釈しないので、{@link regexp}の挿入値としての正規表現パターンを生成するときに利用する。
  */
-regexp.sub = function sub(...args: Parameters<typeof regexp>): {
+regexp.sub = function sub(
+  template: TemplateStringsArray,
+  ...values: RegExpTemplateParam[]
+): {
   source: string;
   flags: RegExpFlags;
 } {
-  let flags: RegExpFlags = '';
-  const source = args[0].raw
+  const arranged = template.raw
     // - エスケープされた文字は残す
     //   `\/\/ aaaaaa`
     //   -> '\\/\\/ aaaaaa'
@@ -92,27 +106,28 @@ regexp.sub = function sub(...args: Parameters<typeof regexp>): {
             : // その他は削除
               '',
       ),
-    )
-    .reduce((r, e, i) => {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- タグ付きテンプレートで挿入される値なので範囲外ではない
-      const value = args[i]!;
-      const pattern =
-        typeof value === 'string'
-          ? // 文字列が指定されたら正規表現の特殊文字をエスケープして挿入。
-            value.replace(/[[\](){}.?+*|^$\\]/g, '\\$&')
-          : ('flags' in value &&
-              // フラグが指定されていればマージ
-              (flags += [...value.flags]
-                // flagsにないものだけを追加
-                .filter(flag => !flags.includes(flag))
-                .join('')),
-            'source' in value
-              ? // 正規表現はそのパターンをそのまま、ただし前後に影響が出ないように`(?:～)`で囲んで挿入
-                `(?:${value.source})`
-              : // 上記以外は除去
-                '');
-      return r.concat(pattern, e);
-    });
+    );
+  let flags: RegExpFlags = '';
+  const source = taggedTemplateBase(arranged, values, value => {
+    if (typeof value === 'string') {
+      // 文字列が指定されたら正規表現の特殊文字をエスケープして挿入。
+      return value.replace(/[[\](){}.?+*|^$\\]/g, '\\$&');
+    }
+    if (typeof value.flags === 'string') {
+      // フラグが指定されていればマージ
+      for (const flag of value.flags) {
+        if (!flags.includes(flag)) {
+          // flagsにないものだけを追加
+          flags += flag;
+        }
+      }
+    }
+    return typeof value.source === 'string'
+      ? // 正規表現はそのパターンをそのまま、ただし前後に影響が出ないように`(?:～)`で囲んで挿入
+        `(?:${value.source})`
+      : // 上記以外は除去
+        '';
+  });
   return { source, flags };
 };
 
