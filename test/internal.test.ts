77467e990f5e36c88967aabbb20e491e74f33b4d
YEBISUYA Sugoroku <yebisuya@gmail.com>
2023-05-12 15:26:10 +0900

    test: デフォルトでexportしていない関数のテストを追加

---
+++ test/internal.test.ts
@@ -0,0 +1,14 @@
+import { taggedTemplateBase } from '../src/taggedTemplateBase';
+import { unescape } from '../src/unescape';
+
+describe('unescape.safe', () => {
+  test('freeze', () => {
+    expect(() => (unescape.safe = () => '')).toThrow();
+  });
+});
+
+describe('taggedTemplateBase', () => {
+  test('empty methods', () => {
+    expect(taggedTemplateBase()`abc${'def'}ghi`).toBe('abcdefghi');
+  });
+});
